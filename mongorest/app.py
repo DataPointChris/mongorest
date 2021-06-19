1d78bce96662e7d7338674b53d51c6e61e728de0
Chris Birch <chrisbirch@live.com>
2021-06-19 15:18:22 -0400

    update: Add .env for mongoclient
    Basic mongo and flask scaffold set up.

--- mongorest/app.py
+++ mongorest/app.py
@@ -1,9 +1,24 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
+from pymongo import MongoClinet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+client = MongoClinet(os.environ.get("MONGODB_URI"))
+app.db = client.mydb
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    if request.method == 'POST':
+        db_entry = request.form.get('form_entry')
+        app.db.mydb.insert({'record'})
+
+    records = [
+        (record['field1'], record['field2']) for record in app.db.entries.find({})
+    ]
+
+    return render_template('index.html', records=records)
 
