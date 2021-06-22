a0494395db4ba0a564b595d48e012fcd67403ff9
Chris Birch <chrisbirch@live.com>
2021-06-22 14:42:42 -0400

    feat: department and role endpoints

--- mongorest/app.py
+++ mongorest/app.py
@@ -11,7 +11,6 @@ from . import sqlitedb
 dotenv.load_dotenv()
 
 
-
 CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 # CLIENT = pymongo.MongoClient(os.environ.get('MONGODB_AWS'))
 # CLIENT = pymongo.MongoClient(os.environ.get('MONGODB_ATLAS'))
@@ -22,37 +21,58 @@ db = mongodb.DBManager(client=CLIENT, database=DATABASE, collection=COLLECTION)
 
 app = Flask(__name__)
 
-
-@app.route('/json/')
-def get_todos_json():
-    todos = db.get_all()
-    return dumps({'todos': todos})
+# db.insert_test_values(50)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def todo_list():
-    if request.method == 'POST':
-        project = request.form.get('project')
-        task = request.form.get('task')
-        description = request.form.get('description')
-        db.insert(project, task, description)
-
-    records = db.get_all()
-    return render_template('index.html', records=records)
+def index():
+    return render_template('index.html')
 
 
-@app.route('/<int:id>', methods=['GET'])
-def get_task_by_id(id):
-    records = db.get_by_id(id)
-    return render_template('index.html', records=records)
+@app.route('/employee/')
+def get_employees():
+    employees = db.get_employee_list()
+    return dumps({'employees': employees})
 
 
-@app.route('/projects/')
-def get_projects_json():
-    projects = db.get_projects()
-    return dumps({'projects': projects})
+@app.route('/employee/<int:id>/')
+def employee_by_id(id):
+    employee = db.get_employee_id(id)
+    return dumps({'employee': employee})
 
 
-if __name__ == '__main__':
-    # db.insert_test_values()
+@app.route('/role/')
+def get_roles():
+    roles = db.get_role_list()
+    return dumps({'roles': roles})
 
+
+@app.route('/role/<string:name>/')
+def role_by_name(name):
+    '''Find employees with this role'''
+    roles = db.get_role_employees(name)
+    return dumps({name: roles})
+
+
+@app.route('/department/')
+def get_departments():
+    departments = db.get_department_list()
+    return dumps({'departments': departments})
+
+
+@app.route('/department/<string:name>/')
+def department_by_name(name):
+    '''Find employees with this department'''
+    departments = db.get_department_employees(name)
+    return dumps({name: departments})
+
+
+# def todo_list():
+#     if request.method == 'POST':
+#         project = request.form.get('project')
+#         task = request.form.get('task')
+#         description = request.form.get('description')
+#         db.insert(project, task, description)
+
+#     records = db.get_employee_list()
+#     return render_template('index.html', records=records)
--- mongorest/mongodb.py
+++ mongorest/mongodb.py
@@ -1,20 +1,33 @@
 from . import junkinserts
 from .fake_employees import create_fake_employees
 
+
 class DBManager:
     def __init__(self, client, database, collection):
         self.client = client
         self.db = self.client[database]
         self.coll = self.db[collection]
 
-    def insert_test_values(self):
-        self.coll.insert_many(create_fake_employees(5))
+    def insert_test_values(self, qty):
+        self.coll.insert_many(create_fake_employees(qty))
 
-    def get_all(self):
-        return list(self.coll.find({}))
+    def get_employee_list(self):
+        return self.coll.find({})
 
-    def get_by_id(self, id):
+    def get_employee_id(self, id):
         return self.coll.find({'id': id})
+
+    def get_role_list(self):
+        return self.coll.distinct('role')
+
+    def get_role_employees(self, name):
+        return self.coll.find({'role': name})
+
+    def get_department_list(self):
+        return self.coll.distinct('department')
+
+    def get_department_employees(self, name):
+        return self.coll.find({'department': name})
 
     def insert(self, project, task, description):
         self.coll.insert_one(
@@ -30,6 +43,3 @@ class DBManager:
 
     def delete(self, id):
         self.coll.delete_one({'_id': id})
-
-    def get_projects(self):
-        return self.coll.distinct('project')
